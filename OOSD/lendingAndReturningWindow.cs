8fb41d761905b638b807941b1f8c369458295ff0
sameera-sampath <sameera.sampath@gmail.com>
2013-12-18 00:06:31 +0530

    Final Beta version
    Group project for Object oriented software development subject. We
    developed Libeary automation system.

---
+++ OOSD/lendingAndReturningWindow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOSD
+{
+    public partial class lendingAndReturningWindow : Form
+    {
+        private MainWindow mWin;
+        Connection connection;
+
+        public lendingAndReturningWindow(MainWindow temp)
+        {
+            InitializeComponent();
+            mWin = temp;
+            connection = new Connection();
+        }
+
+        private void lendButton_Click(object sender, EventArgs e)
+        {
+            DateTime due = connection.lendBook(lendBookIDBox.Text,lendUserIDBox.Text,DateTime.Now);
+            lendBookIDBox.Text = "";
+            lendUserIDBox.Text = "";
+        }
+
+        private void lendUserIDBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            mWin.Enabled = true;
+            base.OnFormClosing(e);
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            DateTime today = new DateTime();
+            DateTime due = connection.returnBook(returnBookIDBox.Text, returnUserIDBox.Text, DateTime.Now);
+            if (DateTime.Compare(due,today)<=0)
+            {
+                TimeSpan diff = today - due;
+
+            }
+            returnBookIDBox.Text = "";
+            returnUserIDBox.Text = "";
+        }
+
+
+    }
+}
